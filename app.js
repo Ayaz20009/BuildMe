fab2eb773c15ae6d8f84daea6ebb28c07e543d2c
Ayaz Shah <ayazshah_20009@yahoo.com>
2016-10-09 20:44:31 +0000

    added routes

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-  res.send("<h1>This is the Homepage</h1>")
+  res.send("<h1>This is the Homepage</h1>");
 });
 
+app.get('/signup',function(req,res){
+	res.send("<h1>Sign up page</h1>");
+});
 
+app.get('/jobs',function(req,res){
+	res.send("<h1>Jobs page</h1>");
+});
+
+app.get('/dashboard',function(req,res){
+	res.send("<h1>dashboard page </h1>");
+});
+
+app.get('/howitworks',function(req,res){
+	res.send("<h1>howitworks page</h1>");
+});
+
+app.get('/searchajob',function(req,res){
+	res.send("<h1>Search a job page</h1>");
+});
 
 app.listen(3000,function(){
   console.log("The frontend server is running on port 3000")
