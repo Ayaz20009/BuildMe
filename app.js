098410a30d6e94dbdb3f211244566154c1aa7b77
Ayaz Shah <ayazshah_20009@yahoo.com>
2016-10-09 05:33:42 +0000

    sample route deleted

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-  res.send("<h1>Hello World</h1>")
+  res.send("<h1>This is the Homepage</h1>")
 });
 
-app.get('/blog/:title' ,function(req,res){
-  var title = req.params.title;
-  var post = posts[title];
-  res.send(post)
-});
+
 
 app.listen(3000,function(){
   console.log("The frontend server is running on port 3000")
