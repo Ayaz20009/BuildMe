8b7d439576b471da00c17167f33e1bb1bf5bfe60
Ayaz Shah <ayazshah_20009@yahoo.com>
2016-10-10 04:26:16 +0000

    removed comments from app.js

--- app.js
+++ app.js
@@ -8,29 +8,6 @@ app.set('views',__dirname + '/views');
 
 app.use(require('./controllers'));
 
-// var index = require('./controllers/index');
-// app.use('/', index)
-
-
-// app.get('/signup',function(req,res){
-// 	res.render('signup')
-// });
-
-// app.get('/jobs',function(req,res){
-// 	res.render('jobs')
-// });
-
-// app.get('/dashboard',function(req,res){
-// 	res.render('dashboard')
-// });
-
-// app.get('/howitworks',function(req,res){
-// 	res.render('HowitWorks')
-// });
-
-// app.get('/searchajob',function(req,res){
-// 	res.render('searchajob')
-// });
 
 app.listen(3000,function(){
   console.log("The frontend server is running on port 3000")
