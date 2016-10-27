bc437fab2c20b40a7e40ca3dd151711a204a1775
susanjiang03 <susanjiang03@gmail.com>
2016-10-27 19:47:56 -0400

    fixed typo

--- app.js
+++ app.js
@@ -3,31 +3,24 @@
 
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize("postgres://pg_user:pg_pass@localhost:5432/buildme_development");
+// var sequelize = new Sequelize("postgres://pg_user:pg_pass@localhost:5432/buildme_development");
 
+// var sql = new Sequelize('buildme_development', 'pg_user', 'pg_pass', {
 
-// sequelize.authenticate().complete(function(err) {
-//     if (err) {
-//       console.log('Unable to connect to the database:', err);
-//     } else {
-//       console.log('Connection has been established successfully.');
-//     }
-// });
-
-var sql = new Sequelize('buildme_development', 'pg_user', 'pg_pass', {
+var sql = new Sequelize('buildme_development', 'postgres', '', {
     host: 'localhost',
     port: 5432,
     dialect: 'postgres'
 });
 
-var test = sql.authenticate()
-    .then(function () {
-        console.log("CONNECTED! ");
-    })
-    .catch(function (err) {
-        console.log("SOMETHING DONE GOOFED");
-    })
-    .done();
+// var test = sql.authenticate()
+//     .then(function () {
+//         console.log("Connection has been established successfully.");
+//     })
+//     .catch(function (err) {
+//         console.log("Unable to connect to the database:" + err);
+//     })
+//     .done();
 
 const express = require('express');
 const app = express();
@@ -42,6 +35,16 @@ app.use(require('./controllers'));
 
 app.listen(3000,function(){
   console.log("The frontend server is running on port 3000")
+
+  //db connection
+  sql.authenticate()
+    .then(function () {
+        console.log("Connection has been established successfully.");
+    })
+    .catch(function (err) {
+        console.log("Unable to connect to the database:" + err);
+    })
+    .done();
 });
 
 
