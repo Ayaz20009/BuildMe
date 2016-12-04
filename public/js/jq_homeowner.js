f73c6b265a1dfade7bf0d3c23673205b9530e44c
susanjiang03 <susanjiang03@gmail.com>
2016-12-03 23:58:33 -0500

    modify create job form,add field of times

--- public/js/jq_homeowner.js
+++ public/js/jq_homeowner.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+
+	$('#btnASAP').click(function(){
+
+		if($(this).hasClass("btn-danger")){
+
+		   $(this).removeClass("btn-danger").addClass("btn-default").html("ASAP").val("TRUE");
+		}
+		else{
+		   $(this).addClass("btn-danger").removeClass("btn-default").html("&#10003;  ASAP").val("FALSE");
+		}
+	});
+
    //click btn Delete
   $(".btnDeleteJob").click(function(){
 
@@ -7,12 +19,17 @@ $(document).ready(function(){
     $('#modal_delete_job').find('.modal-body').html(jobInfo_html);
   });
 
+
  
  //add class btn-default to current option 
 var url      = window.location.href;
 var urlSplit = url.split("/");
 var path = urlSplit[3] + "/" + urlSplit[4];
 $('.sidebar a[href*="'+ path +'"]').addClass("btn-success").siblings().removeClass("btn-success");
+
+
+
+
 
 });
 
