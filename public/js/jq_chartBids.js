0ee9dd715016eb731bc6842fd705c440f47ad0be
susanjiang03 <susanjiang03@gmail.com>
2016-12-07 02:32:24 -0500

    add points example for visualization dual Y bar chart

--- public/js/jq_chartBids.js
+++ public/js/jq_chartBids.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 google.charts.load('current', {'packages':['bar']});
 
 google.charts.setOnLoadCallback(drawChartBids);
@@ -12,7 +12,7 @@ function drawChartBids() {
 
      // 	alert(response);
 
-        var data  = [['Name', 'Estimate Cost', 'Estimate Days']];
+        var data  = [['Name', 'Estimate Cost', 'Estimate Days', 'points']];
 
         for(var i in response){
 
@@ -26,25 +26,32 @@ function drawChartBids() {
                estDays = parseInt(row.estDays);
             if(row.estHours)
                estDays  += (parsefloat(row.estHours)/24.0).toFixed(1);
-             data.push([name, estCost, estDays]);
+
+             //generate random points for example use
+             var points = parseInt(Math.random() * 10000);
+
+             $('[id=bidID_'+ row.bidID+']').find('[name=points]').text(points);
+             data.push([name, estCost, estDays, points ]);
         }
 
       var dataTable = new google.visualization.arrayToDataTable(data);
 
         var options = {
-          width: document.getElementById('bids').width*0.9,
+          width: document.getElementById('bids').width*0.8,
           chart: {
             title: 'Compare Estimate Cost and Estimate Days',
             subtitle: 'Estimate Cost on the left, Estimate Days on the right'
           },
           series: {
             0: { axis: 'Estimate Cost' }, // Bind series 0 to an axis named 'Estimate Cost'.
-            1: { axis: 'Estimate Days' } // Bind series 1 to an axis named ''Estimate Days'.
+            1: { axis: 'Estimate Days' },
+            2: { axis: 'Points' },// Bind series 1 to an axis named ''Estimate Days'.
           },
           axes: {
             y: {
-              "Estimate Cost": {label: ''}, // Left y-axis.
-              'Estimate Days' : {side: 'right', label: ''} // Right y-axis.
+              "Estimate Cost": {label: 'Estimate Cost'}, // Left y-axis.
+              "Estimate Days" : {side: 'right', label: 'Estimate Days'},
+              "Points": {label: 'Points'}// Right y-axis.
             }
           }
         };
@@ -59,5 +66,5 @@ function drawChartBids() {
 
 
 
-// });
+});
 
